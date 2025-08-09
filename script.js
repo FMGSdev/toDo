95e0ff7e15614e81cb40ac9304918b9fbd37db80
Fabio Melo <fabiomelogs85@gmail.com>
2025-08-08 21:47:12 -0300

    Começando com os eventos

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-let nomeUsuario = "Fabio";
-const cpf = "11915792711";
+const lista = document.querySelector("[data-list]");
+
+const novaTarefa = document.querySelector("[data-form-button]");
+
+const criarTarefa = (evento) =>{
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    console.log(valor);
+
+}
+
+
+//Evento
+novaTarefa.addEventListener('click', criarTarefa);
+    
+
+//Espera um evento, e o que vai acontecer
+
+//Eventos
+//Qual o tipo do evento - click
+//O que vai acontecer - nova tarefa
+//Onde irá ocorrer o evento - button
+
+
+
+
+
