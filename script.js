cf416bbda582c399814b37f818cc207ede19466f
Fabio Melo <fabiomelogs85@gmail.com>
2025-08-18 18:20:14 -0300

    pequenos ajustes

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-const lista = document.querySelector("[data-list]");
-
-const novaTarefa = document.querySelector("[data-form-button]");
-
-const criarTarefa = (evento) =>{
+const criarTarefa = (evento) => {
     evento.preventDefault();
+    
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
-    console.log(valor);
+
+    const tarefa = document.createElement(`li`);
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    tarefa .appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+
+
+    input.value = "";
+}
+
+
+
+// Evento
+const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir';
+
+    botaoConclui.addEventListener('click', ()=> {
+        console.log('fui clicado');
+
+    });
+    return botaoConclui;
 
 }
 
 
-//Evento
-novaTarefa.addEventListener('click', criarTarefa);
-    
 
-//Espera um evento, e o que vai acontecer
-
-//Eventos
-//Qual o tipo do evento - click
-//O que vai acontecer - nova tarefa
-//Onde irá ocorrer o evento - button
 
 
 
