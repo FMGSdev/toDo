9f165e62e856ead2c5a5bd9b87667b9e7829f27b
unknown <fabiomelogs85@gmail.com>
2025-08-21 16:54:48 -0300

    incluido botão deletar

--- anota.md
+++ anota.md
@@ -1,9 +1,14 @@
-* COMANDOS PRINCIPAIS DO GITHUB
-1. git init
-2. git add .
-3. git status
-4. git branch -M main
-5. git commit -m "First project"
-6. git push
+COMANDOS PRINCIPAIS DO GITHUB
+git init
+git add .
+git status
+git branch -M main
+git commit -m "Subindo a primeira versão"
+git push
 
-*DOCUMENTAÇÃO DO TODOgit
+DOCUMENTAÇÃO DO TODO
+createElement() -> Cria um novo elemento. appendChild() -> Insere uma tag filha na tag pai. innerHTML -> Insere um elemento dentro de outro elemento na mesma hierarquia. classList -> Atributo que acessa a lista de classes da tag add() -> Adiciona alguma coisa.
+
+Preciso pegar a const conteudo, inserir ela dentro de uma tag li e inserir essa tag li na ul.
+
+Preciso incluir um botão no item toda vez que eu criar um novo item.
--- script.js
+++ script.js
@@ -3,21 +3,20 @@ const criarTarefa = (evento) => {
     
     const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
-    const valor = input.value;
+    const valor = input.value;  
 
-    const tarefa = document.createElement(`li`);
+    const tarefa = document.createElement('li');
     tarefa.classList.add('task');
     const conteudo = `<p class="content">${valor}</p>`;
-
+    
     tarefa.innerHTML = conteudo;
-    tarefa .appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
     lista.appendChild(tarefa);
 
-
+  
     input.value = "";
 }
-
-
 
 // Evento
 const novaTarefa = document.querySelector("[data-form-button]");
@@ -27,15 +26,47 @@ const BotaoConclui = () => {
     const botaoConclui = document.createElement('button');
 
     botaoConclui.classList.add('check-button');
-    botaoConclui.innerText = 'concluir';
+    botaoConclui.innerText = 'concluir'
 
-    botaoConclui.addEventListener('click', ()=> {
-        console.log('fui clicado');
-
-    });
+    botaoConclui.addEventListener('click', concluirTarefa);
     return botaoConclui;
 
+};
+
+const concluirTarefa = (evento) => {
+    const eventoAcionado = evento.target;
+
+    const tarefaCompleta = eventoAcionado.parentElement;
+
+    tarefaCompleta.classList.toggle('done'); 
+
+
+};
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button');
+
+    botaoDeleta.innerText = 'deletar';
+    botaoDeleta.addEventListener('click', deletarTarefa);
+
+    return botaoDeleta;
 }
+
+const deletarTarefa = (evento) =>{
+    const deletaEvento = evento.target;
+
+    const tarefaDeletada = deletaEvento.parentElement;
+    tarefaDeletada.remove();
+}
+
+
+
+
+// Eventos  
+// Qual o tipo do evento - click
+// O que vai acontecer - nova tarefa 
+// Onde irá ocorrer o evento - button
+
 
 
 
