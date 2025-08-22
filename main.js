e4e227bda91a4c71fd4984bd50dddf3539a3f140
Fabio Melo <fabiomelogs85@gmail.com>
2025-08-22 20:56:23 -0300

    subindo a versão final

---
+++ main.js
@@ -0,0 +1,46 @@
+import BotaoConclui from "./components/concluiTarefa.js";
+import BotaoDeleta from "./components/deletaTarefa.js";
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    
+    const lista = document.querySelector("[data-list]");
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;  
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+    
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+    lista.appendChild(tarefa);
+
+  
+    input.value = "";
+}
+
+// Evento
+const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa);
+
+
+
+
+
+
+
+
+// Eventos  
+// Qual o tipo do evento - click
+// O que vai acontecer - nova tarefa 
+// Onde irá ocorrer o evento - button
+
+
+
+
+
+
+
+
+
